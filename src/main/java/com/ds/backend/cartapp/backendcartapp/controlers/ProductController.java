0a2e617894692befebeadde62b9c949ecbf7300b
Diegantos <diegantos8@gmail.com>
2023-07-12 10:54:57 +0200

    CrossOrigin

--- src/main/java/com/ds/backend/cartapp/backendcartapp/controlers/ProductController.java
+++ src/main/java/com/ds/backend/cartapp/backendcartapp/controlers/ProductController.java
@@ -3,12 +3,14 @@ package com.ds.backend.cartapp.backendcartapp.controlers;
 import com.ds.backend.cartapp.backendcartapp.models.entities.Product;
 import com.ds.backend.cartapp.backendcartapp.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     @Autowired
